8c63a990a8ed34a15f81cb2ce6536265e5f81e38
OASISAsx <122677372+OASISAsx@users.noreply.github.com>
2026-04-26 22:34:21 +0700

    Fix menu auth guard module dependencies

--- apps/backend/src/auth/auth.module.ts
+++ apps/backend/src/auth/auth.module.ts
@@ -18,6 +18,6 @@ import { JwtAuthGuard } from "./jwt-auth.guard";
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtAuthGuard],
-  exports: [JwtAuthGuard]
+  exports: [JwtAuthGuard, JwtModule]
 })
 export class AuthModule {}
